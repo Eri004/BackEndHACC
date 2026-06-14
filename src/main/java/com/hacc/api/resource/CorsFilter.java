caf3a7b85c43d178f925b6f60e2d935c80155a93
Erick <wayofthedragon32@gmail.com>
2026-06-14 10:43:04 -0500

    agregados imports que faltaban a corsfilter

--- src/main/java/com/hacc/api/resource/CorsFilter.java
+++ src/main/java/com/hacc/api/resource/CorsFilter.java
@@ -1,7 +1,14 @@
 package com.hacc.api.resource;
+
+import java.io.IOException;
+
 import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.container.PreMatching;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
 
 @Provider
 @PreMatching
